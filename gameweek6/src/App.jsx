5acd73097d209d9315d29d5400ea0589d6434bdb
Jia <jovsofjdo@gmail.com>
2023-08-16 14:58:38 -0700

    Added .gitignore and set up react assets

---
+++ gameweek6/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>This is my game</p>
+    </>
+  )
+}
+
+export default App
