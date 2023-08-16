a7e37fc17d0355c568be2107eaf43787c2a566a2
Jia <jovsofjdo@gmail.com>
2023-08-16 15:42:28 -0700

    Added keyboard and board components. Modified CSS file.

--- gameweek6/src/App.jsx
+++ gameweek6/src/App.jsx
@@ -5,9 +5,13 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <p>This is my game</p>
-    </>
+    <div className='App'>
+      <nav>
+        <h1>Wordle</h1>
+      </nav>
+      <Board />
+      <Keyboard />
+    </div>
   )
 }
 
