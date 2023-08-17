e8dfad6e4497f28ca5f9a7375921718454952dbd
Jia <jovsofjdo@gmail.com>
2023-08-17 13:53:13 -0700

    Added key.jsx component

--- gameweek6/src/components/Keyboard.jsx
+++ gameweek6/src/components/Keyboard.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
 
 function Keyboard() {
+    //set up each keyboard row as an array
     const keys1 = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'];
     const keys2 = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L']
     const keys3 = ['Z', 'X', 'C', 'V', 'B', 'N', 'M']
   return (
     <div className='keyboard'>
-        <div className='line1'></div>
-        <div className='line2'></div>
-        <div className='line3'></div>
+    {/* make each keyboard row a div so it will be displayed like a keyboard*/}
+        <div className='line1'>
+            {keys1.map((key)=>{
+                return <div>{key}</div>
+            })}
+        </div>
+        <div className='line2'>
+            {keys2.map((key) => {
+                return <div>{key}</div>
+            })}
+        </div>
+        <div className='line3'>
+            {keys3.map((key) => {
+                return <div>{key}</div>
+            })}
+        </div>
     </div>
   )
 }
