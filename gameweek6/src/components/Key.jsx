a76a97882dfae4a82a7914e471285649d9b9df82
Jia <jovsofjdo@gmail.com>
2023-08-17 15:51:10 -0700

    Set up condition for if player presses ENTER

--- gameweek6/src/components/Key.jsx
+++ gameweek6/src/components/Key.jsx
@@ -1,29 +1,35 @@
 import React, { useContext } from 'react'
 import { AppContext } from '../App'
 
-
 //Sets conditional logic for bigKeys (Delete, Enter) and letter keys. 
-//Set logic
-function Key({keyVal, bigKey}) {
+//Set logic for updating the board as you play
+function Key({ keyVal, bigKey }) {
     //import board state and attempt state
-    const { board, setBoard, currentAttempt, setCurrentAttempt} = useContext(AppContext)
+    const { board, setBoard, currentAttempt, setCurrentAttempt } = useContext(AppContext)
+
     //selects letter by grabbing keyVal prop from Keyboard and setting it to the new state
     const selectLetter = () => {
-        //declare newBoard and set it to current state of the board
-        const newBoard = [...board]
-        //access useState currentAttempt object, get current letter input from keyVal prop, and put it on the board
-        newBoard[currentAttempt.attempt][currentAttempt.letterPosition] = keyVal
-        //update current state of board to include current letter input
-        setBoard(newBoard)
-        //update current attempt state and move the position of the letter to the next square in the row
-        setCurrentAttempt({...currentAttempt, letterPosition: currentAttempt.letterPosition + 1})
+        if (keyVal === 'ENTER') {
+
+        } else {
+            //if player enters a 5th letter in the row, exit the function
+            if (currentAttempt.letterPosition > 4) return
+            //declare newBoard and set it to current state of the board
+            const newBoard = [...board]
+            //access useState currentAttempt object, get current letter input from keyVal prop, and put it on the board
+            newBoard[currentAttempt.attempt][currentAttempt.letterPosition] = keyVal
+            //update current state of board to include current letter input
+            setBoard(newBoard)
+            //update current attempt state and move the position of the letter to the next square in the row
+            setCurrentAttempt({ ...currentAttempt, letterPosition: currentAttempt.letterPosition + 1 })
+        }
     }
-  return (
-//   use keyVal prop in div and set conditional id for bigKeys to apply CSS conditionally
-    <div className="key" id={bigKey && "big"} onClick={selectLetter}>
-        {keyVal}
-    </div>
-  )
+    return (
+        //   use keyVal prop in div and set conditional id for bigKeys to apply CSS conditionally
+        <div className="key" id={bigKey && "big"} onClick={selectLetter}>
+            {keyVal}
+        </div>
+    )
 }
 
 export default Key
